f814fcf78cb6d00e0c0205d3270193b25ae32e3e
IconicXDivyansh <divyanshpingle900@gmail.com>
2024-10-18 19:41:14 +0530

    added auth functionality in the backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
-console.log("server >...")
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// connect to database
+connectDB();
+
+const app = express();
+
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => { 
+    res.json({message: 'welcome to support desk'});
+
+})
+
+//Routes 
+app.use('/api/users', require('./routes/userRoutes'));
+
+// error Handler
+app.use(errorHandler);
+
+app.listen(PORT, () => { 
+    console.log(`Server is running on port ${PORT}`);
+})
