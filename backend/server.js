1b159870f8a1481b04bbd40216c5a5aef9021c8a
IconicXDivyansh <divyanshpingle900@gmail.com>
2024-10-22 15:23:22 +0530

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const colors = require('colors');
+const path = require('path');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db');
@@ -15,16 +16,32 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => { 
-    res.json({message: 'welcome to support desk'});
-
-})
 
 //Routes 
 app.use('/api/users', require('./routes/userRoutes'));
 
+app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// serve frontend
+if (process.env.NODE_ENV === 'production') { 
+    // set build folder as static folder
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) => { 
+        res.sendFile(__dirname, '../frontend', 'build', 'index.html');
+    })
+} else {
+    app.get('/', (req, res) => { 
+        res.json({message: 'welcome to support desk'});
+    
+    })
+}
+
+
 // error Handler
 app.use(errorHandler);
+
+
 
 app.listen(PORT, () => { 
     console.log(`Server is running on port ${PORT}`);
